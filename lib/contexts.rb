5b5efd5b108c320dc6663d312336e3b281808e9a
Alban Scholer <alban.scholer@gmail.com>
2012-10-17 23:57:47 +0200

    updates

--- lib/contexts.rb
+++ lib/contexts.rb
@@ -2,13 +2,15 @@ require 'data.rb'
 require 'roles.rb'
 
 class LogIntoDropboxAccount
-  def initialize(app_key, app_secret, on_success=lambda({}), on_failure=lambda({}))
+  def initialize(app_key, app_secret, on_success=lambda {}, on_failure=lambda {})
     @app_key=app_key
     @app_secret=app_secret
     @on_success=on_success
     @on_failure=on_failure 
   end
   def run
-    
+    account = DropboxAccount.new(app_key:@app_key, app_secret:@app_secret, id:1)
+    account.extend(Store)
+    account.store
   end
 end
--- spec/contexts/setup_account_spec.rb
+++ spec/contexts/setup_account_spec.rb
@@ -1,15 +1,22 @@
 require 'data.rb'
 require 'contexts.rb'
-
+require 'repository'
 APP_KEY='APP_KEY'
 APP_SECRET='APP_SECRET'
 
+def auth_url=(auth_url)
+  @auth_url = auth_url
+end
 describe LogIntoDropboxAccount do
   before do
-    @context = LogIntoDropboxAccount.new(APP_KEY, APP_SECRET)
+    @auth_url=nil
+    @context = LogIntoDropboxAccount.new(APP_KEY, APP_SECRET, method(:auth_url=))
   end
-
-  it 'creates a pending DropboxAccount and return notify success callback with auth url' do
-    
+  
+  it 'creates a pending DropboxAccount' do
+    @context.run
+    Repository[DropboxAccount].size.should be(1)
   end 
+  it 'should notify success callback with auth_url as argument' do
+  end
 end
